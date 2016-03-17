19cffa8da30ef7a254742ce4fb097f777ce93768
Dan Gray <dan@sinensis.co.uk>
2016-03-17 14:32:02 +0000

    Add config enabled checks

--- js/adapt-contrib-narrative-audio.js
+++ js/adapt-contrib-narrative-audio.js
@@ -319,7 +319,7 @@ define(function(require) {
             Adapt.trigger('notify:popup', popupObject);
 
             ///// Audio /////
-            if (this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isEnabled && this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
                 // Trigger audio
                 Adapt.trigger('audio:playAudio', currentItem._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
             }
@@ -346,7 +346,7 @@ define(function(require) {
             if (Adapt.device.screenSize === 'large') {
                 var currentItem = this.getCurrentItem(stage);
 
-                if (this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
+                if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isEnabled && this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
                     // Trigger audio
                     Adapt.trigger('audio:playAudio', currentItem._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
                 }
@@ -397,7 +397,7 @@ define(function(require) {
         // Reduced text
         replaceText: function(value) {
             // If enabled
-            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && vthis.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 // Change component title and body
                 if(value == 0) {
                     if (this.model.get('displayTitle')) {
